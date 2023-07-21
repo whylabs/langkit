c10df41bc7efd0dfd040828f1bf38f5e489d681a
Felipe Adachi <felipeadachi@gmail.com>
2023-07-21 14:59:20 -0300

    linting

--- langkit/tests/test_nlp_scores.py
+++ langkit/tests/test_nlp_scores.py
@@ -1,12 +1,15 @@
 import whylogs as why
 import pytest
 
+
 @pytest.mark.load
 def test_bleu_score():
     from langkit import nlp_scores  # noqa
     from whylogs.experimental.core.udf_schema import udf_schema
 
-    nlp_scores.init(scores=["bleu"], corpus="The quick brown fox jumps over the lazy dog")
+    nlp_scores.init(
+        scores=["bleu"], corpus="The quick brown fox jumps over the lazy dog"
+    )
     text_schema = udf_schema()
     profile = why.log(
         {"response": "The quick dog jumps over the lazy brown fox"}, schema=text_schema
