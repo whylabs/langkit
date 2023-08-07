29fc23bb9156984810ab669b30bd22c92dec16a6
Richard Rogers <richard@whylabs.ai>
2023-08-07 14:32:00 -0700

    pre-commit

--- langkit/tests/test_textstat.py
+++ langkit/tests/test_textstat.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import langkit.textstat as ts
 import whylogs as why
@@ -11,13 +10,13 @@ def test_textstat():
             "prompt": [
                 "timely text propagated prolifically",
                 "articulate artichokes aggravated allergically",
-                "just some words"
+                "just some words",
             ],
             "response": [
                 "words words everywhere but not a thought to think",
                 "strawberries are not true fruits",
-                "this is not my response"
-            ]
+                "this is not my response",
+            ],
         }
     )
     schema_names = set([s for _, s in ts._udfs_to_register])
@@ -27,7 +26,11 @@ def test_textstat():
         for stat, stat_schema in ts._udfs_to_register:
             for column in ["prompt", "response"]:
                 if stat_schema in {"", schema_name}:
-                    dist = view.get_column(f"{column}.{stat}").get_metric("distribution").to_summary_dict()
+                    dist = (
+                        view.get_column(f"{column}.{stat}")
+                        .get_metric("distribution")
+                        .to_summary_dict()
+                    )
                     assert "mean" in dist
                 else:
                     assert view.get_column(f"{column}.{stat}") is None
