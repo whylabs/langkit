e5cce3ab9e279dd81ea7784d7796904e0894a41b
Richard Rogers <richard@whylabs.ai>
2023-08-08 14:55:15 -0700

    pull toxicity out into callable/wrapper

--- langkit/toxicity.py
+++ langkit/toxicity.py
@@ -1,33 +1,37 @@
 from typing import Optional
 
-from whylogs.core.stubs import pd
 from whylogs.experimental.core.udf_schema import register_dataset_udf
 from . import LangKitConfig
 
 lang_config = LangKitConfig()
+_prompt = lang_config.prompt_column
+_response = lang_config.response_column
 
 _toxicity_model_path = "martin-ha/toxic-comment-model"
 _toxicity_tokenizer = None
 _toxicity_pipeline = None
 
 
-def toxicity(text):
+def toxicity(text: str) -> float:
     if _toxicity_pipeline is None or _toxicity_tokenizer is None:
         raise ValueError("toxicity score must initialize the pipeline first")
 
-    index = text.columns[0] if isinstance(text, pd.DataFrame) else list(text.keys())[0]
-    scores = []
-    for input in text[index]:
-        result = _toxicity_pipeline(
-            input, truncation=True, max_length=_toxicity_tokenizer.model_max_length
-        )
-        toxicity_score = (
-            result[0]["score"]
-            if result[0]["label"] == "toxic"
-            else 1 - result[0]["score"]
-        )
-        scores.append(toxicity_score)
-    return scores
+    result = _toxicity_pipeline(
+        text, truncation=True, max_length=_toxicity_tokenizer.model_max_length
+    )
+    return (
+        result[0]["score"] if result[0]["label"] == "toxic" else 1 - result[0]["score"]
+    )
+
+
+@register_dataset_udf([_prompt], f"{_prompt}.toxicity")
+def prompt_toxicity(text):
+    return [toxicity(t) for t in text[_prompt]]
+
+
+@register_dataset_udf([_response], f"{_response}.toxicity")
+def response_toxicity(text):
+    return [toxicity(t) for t in text[_response]]
 
 
 def init(model_path: Optional[str] = None):
@@ -45,9 +49,6 @@ def init(model_path: Optional[str] = None):
     _toxicity_pipeline = TextClassificationPipeline(
         model=model, tokenizer=_toxicity_tokenizer
     )
-
-    for column in [lang_config.prompt_column, lang_config.response_column]:
-        register_dataset_udf([column], udf_name=f"{column}.toxicity")(toxicity)
 
 
 init()
