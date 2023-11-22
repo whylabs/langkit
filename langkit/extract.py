5fea352c38ac4d378af0cbc9d75a7598b266d679
felipe207 <felipe@whylabs.ai>
2023-11-22 18:01:06 -0300

    refactor

--- langkit/extract.py
+++ langkit/extract.py
@@ -1,16 +1,17 @@
 import pandas as pd
-from typing import Any, Dict, Optional
-from whylogs.experimental.core.udf_schema import udf_schema
+from typing import Any, Dict, Optional, Union
+from whylogs.experimental.core.udf_schema import udf_schema, UdfSchema
 
 
 def extract(
-    pandas: Optional[pd.DataFrame] = None,
-    row: Optional[Dict[str, Any]] = None,
+    data: Union[pd.DataFrame, Dict[str, Any]],
+    schema: Optional[UdfSchema] = None,
 ):
-    schema = udf_schema()
-    if pandas is not None:
-        df_enhanced, _ = schema.apply_udfs(pandas=pandas)
+    if schema is None:
+        schema = udf_schema()
+    if isinstance(data, pd.DataFrame):
+        df_enhanced, _ = schema.apply_udfs(pandas=data)
         return df_enhanced
-    if row is not None:
-        _, row_enhanced = schema.apply_udfs(row=row)
+    elif isinstance(data, dict):
+        _, row_enhanced = schema.apply_udfs(row=data)
         return row_enhanced
--- langkit/tests/test_extract.py
+++ langkit/tests/test_extract.py
@@ -1,5 +1,6 @@
 import langkit
 import pandas as pd
+from whylogs.experimental.core.udf_schema import UdfSchema, UdfSpec
 
 
 def test_extract_pandas():
@@ -7,7 +8,7 @@ def test_extract_pandas():
 
     textstat.init()
     df = pd.DataFrame({"prompt": ["I love you", "I hate you"]})
-    enhanced_df = langkit.extract(pandas=df)
+    enhanced_df = langkit.extract(data=df)
     assert "prompt.flesch_reading_ease" in enhanced_df.columns
 
 
@@ -16,7 +17,7 @@ def test_extract_row():
 
     regexes.init()
     row = {"prompt": "I love you", "response": "address: 123 Main St."}
-    enhanced_row = langkit.extract(row=row)
+    enhanced_row = langkit.extract(data=row)
     assert enhanced_row.get("response.has_patterns") == "mailing address"
     assert not enhanced_row.get("prompt.has_patterns")
 
@@ -27,7 +28,21 @@ def test_extract_light_metrics():
     light_metrics.init()
 
     row = {"prompt": "I love you", "response": "address: 123 Main St."}
-    enhanced_row = langkit.extract(row=row)
+    enhanced_row = langkit.extract(row)
     assert enhanced_row.get("response.has_patterns") == "mailing address"
     assert not enhanced_row.get("prompt.has_patterns")
     assert "prompt.flesch_reading_ease" in enhanced_row.keys()
+
+
+def test_extract_with_custom_schema():
+    schema = UdfSchema(
+        udf_specs=[
+            UdfSpec(
+                column_names=["prompt"],
+                udfs={"prompt.customfeature": lambda x: x["prompt"]},
+            )
+        ],
+    )
+    row = {"prompt": "I love you", "response": "address: 123 Main St."}
+    enhanced_row = langkit.extract(row, schema=schema)
+    assert enhanced_row.get("prompt.customfeature") == "I love you"
