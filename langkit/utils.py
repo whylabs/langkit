3c32b2ce7eb10248125f3c565cce2d894d9d8345
Felipe Adachi <felipeadachi@gmail.com>
2024-01-19 16:28:19 -0300

    Add PII module (#217)
    * integrate - presidio pii analyzer
    * unregister bug fix and add example
    * docs and formatting
    ---------
    
    Co-authored-by: felipe207 <felipe@whylabs.ai>
    Co-authored-by: Jamie Broomall <88007022+jamie256@users.noreply.github.com>

--- langkit/utils.py
+++ langkit/utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 from langkit import lang_config
 import string
 import random
@@ -15,3 +16,21 @@ def _get_data_home() -> str:
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return "".join(random.choice(chars) for _ in range(size))
+
+
+def _unregister_metric_udf(old_name: str, namespace: Optional[str] = ""):
+    from whylogs.experimental.core.udf_schema import _multicolumn_udfs
+
+    if _multicolumn_udfs is None or namespace not in _multicolumn_udfs:
+        return
+
+    new_multicolumn_udfs = []
+    for udf in _multicolumn_udfs[namespace]:
+        udfs_cols = list(udf.udfs.keys())
+        if udfs_cols and udfs_cols[0] != old_name:
+            new_multicolumn_udfs.append(udf)
+        # added to account for multicolumn udfs
+        elif udf.udf and udf.prefix != old_name:
+            new_multicolumn_udfs.append(udf)
+
+    _multicolumn_udfs[namespace] = new_multicolumn_udfs
