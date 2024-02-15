da91cd484c6d9fdb46d5846df6f328c274e07628
Anthony Naddeo <anthony@whylabs.ai>
2024-02-15 14:44:02 -0800

    Add additional cache_asset functions, share single transformer cache logic
    There were metrics that weren't using the utility cached transformer
    method despite using the same transformer. This removes the duplicated
    references to that transformer creation, as well as adds a cache_assets
    hook for the themes module so it behaves when we use the
    cache_assets=False flag through workflows.

--- langkit/metrics/input_output_similarity.py
+++ langkit/metrics/input_output_similarity.py
@@ -1,4 +1,4 @@
-from functools import partial
+from functools import cache, partial
 from typing import Optional
 
 import pandas as pd
@@ -6,26 +6,24 @@ import pandas as pd
 from langkit.core.metric import Metric, SingleMetric, SingleMetricResult, UdfInput
 from langkit.metrics.embeddings_types import EmbeddingEncoder, TransformerEmbeddingAdapter
 from langkit.metrics.embeddings_utils import compute_embedding_similarity
-from langkit.metrics.util import DynamicLazyInit
 from langkit.transformer import sentence_transformer
 
-__default_transformer_name = "sentence-transformers/all-MiniLM-L6-v2"
-__transformer_embedding = DynamicLazyInit[str, TransformerEmbeddingAdapter](
-    lambda transformer_name: TransformerEmbeddingAdapter(sentence_transformer.value(transformer_name))
-)
+
+@cache
+def _get_encoder(embedding_encoder: Optional[EmbeddingEncoder] = None):
+    return embedding_encoder or TransformerEmbeddingAdapter(sentence_transformer())
 
 
 def input_output_similarity_metric(
     input_column_name: str = "prompt", output_column_name: str = "response", embedding_encoder: Optional[EmbeddingEncoder] = None
 ) -> Metric:
-    encoder = embedding_encoder or __transformer_embedding.value(__default_transformer_name)
-
     def cache_assets():
-        __transformer_embedding.value(__default_transformer_name)
+        _get_encoder(embedding_encoder)
 
     def udf(text: pd.DataFrame) -> SingleMetricResult:
         in_np = UdfInput(text).to_list(input_column_name)
         out_np = UdfInput(text).to_list(output_column_name)
+        encoder = _get_encoder(embedding_encoder)
         similarity = compute_embedding_similarity(encoder, in_np, out_np)
 
         if len(similarity.shape) == 1:
--- langkit/transformer.py
+++ langkit/transformer.py
@@ -1,8 +1,21 @@
+from functools import cache
+from typing import Tuple
+
 import torch
 from sentence_transformers import SentenceTransformer
 
-from langkit.metrics.util import DynamicLazyInit
 
-sentence_transformer: DynamicLazyInit[str, SentenceTransformer] = DynamicLazyInit(
-    lambda model_name: SentenceTransformer(model_name, device="cuda" if torch.cuda.is_available() else "cpu")
-)
+@cache
+def sentence_transformer(
+    name_revision: Tuple[str, str] = ("all-MiniLM-L6-v2", "44eb4044493a3c34bc6d7faae1a71ec76665ebc6"),
+) -> SentenceTransformer:
+    """
+    Returns a SentenceTransformer model instance.
+
+    The intent of this function is to cache the SentenceTransformer instance to avoid
+    multple instances being created all over langkit, and have a single place that
+    can be used to change the transformer name for the metrics that default to the same one.
+    """
+    transformer_name, revision = name_revision
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    return SentenceTransformer(transformer_name, revision=revision, device=device)
