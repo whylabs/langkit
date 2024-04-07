20243e0545911f59bad0c1f83eb0b82fbf7c12bb
Anthony Naddeo <anthony@whylabs.ai>
2024-04-07 12:20:12 -0700

    Update asset caching logic for toxicity model

--- langkit/metrics/toxicity.py
+++ langkit/metrics/toxicity.py
@@ -22,23 +22,34 @@ def __toxicity(pipeline: TextClassificationPipeline, max_length: int, text: List
     return [result["score"] if result["label"] == "toxic" else 1.0 - result["score"] for result in results]  # type: ignore
 
 
-__model_path = "martin-ha/toxic-comment-model"
+_model_path = "martin-ha/toxic-comment-model"
+_revision = "9842c08b35a4687e7b211187d676986c8c96256d"
+
+
+def _cache_assets():
+    AutoModelForSequenceClassification.from_pretrained(_model_path, revision=_revision)
+    AutoTokenizer.from_pretrained(_model_path, revision=_revision)
 
 
 @lru_cache
 def _get_tokenizer() -> PreTrainedTokenizerBase:
-    return AutoTokenizer.from_pretrained(__model_path)
+    return AutoTokenizer.from_pretrained(_model_path, local_files_only=True, revision=_revision)
 
 
 @lru_cache
 def _get_pipeline() -> TextClassificationPipeline:
     use_cuda = torch.cuda.is_available() and not bool(os.environ.get("LANGKIT_NO_CUDA", False))
-    model: PreTrainedTokenizerBase = AutoModelForSequenceClassification.from_pretrained(__model_path)
+    model: PreTrainedTokenizerBase = AutoModelForSequenceClassification.from_pretrained(
+        _model_path, local_files_only=True, revision=_revision
+    )
     tokenizer = _get_tokenizer()
     return TextClassificationPipeline(model=model, tokenizer=tokenizer, device=0 if use_cuda else -1)
 
 
 def toxicity_metric(column_name: str) -> Metric:
+    def cache_assets():
+        _cache_assets()
+
     def init():
         _get_pipeline()
 
@@ -51,7 +62,9 @@ def toxicity_metric(column_name: str) -> Metric:
         metrics = __toxicity(_pipeline, max_length, col)
         return SingleMetricResult(metrics=metrics)
 
-    return SingleMetric(name=f"{column_name}.toxicity.toxicity_score", input_names=[column_name], evaluate=udf, init=init)
+    return SingleMetric(
+        name=f"{column_name}.toxicity.toxicity_score", input_names=[column_name], evaluate=udf, init=init, cache_assets=cache_assets
+    )
 
 
 prompt_toxicity_metric = partial(toxicity_metric, "prompt")
