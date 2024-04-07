b3ecc2175c8874628eb7daa3f672e06b153a207f
Anthony Naddeo <anthony@whylabs.ai>
2024-04-06 17:29:53 -0700

    Refactor model output processing code

--- langkit/metrics/topic_onnx.py
+++ langkit/metrics/topic_onnx.py
@@ -4,7 +4,7 @@
 
 from dataclasses import dataclass
 from functools import lru_cache, partial
-from typing import Any, Dict, List, Optional
+from typing import List, Optional, TypedDict
 
 import pandas as pd
 import torch
@@ -12,7 +12,7 @@ from optimum.modeling_base import PreTrainedModel
 from optimum.onnxruntime import ORTModelForSequenceClassification
 from transformers import AutoTokenizer, Pipeline, PreTrainedTokenizerBase, pipeline  # type: ignore
 
-from langkit.core.metric import MetricCreator, MultiMetric, MultiMetricResult
+from langkit.core.metric import MetricCreator, MultiMetric, MultiMetricResult, UdfInput
 
 __default_topics = [
     "medicine",
@@ -27,7 +27,8 @@ _hypothesis_template = "This example is about {}"
 def _download_assets():
     ORTModelForSequenceClassification.from_pretrained(
         "MoritzLaurer/xtremedistil-l6-h256-zeroshot-v1.1-all-33",
-        file_name="onnx/model_quantized.onnx",
+        subfolder="onnx",
+        file_name="model.onnx",
         export=False,
     )
 
@@ -39,7 +40,8 @@ def _get_tokenizer() -> PreTrainedTokenizerBase:
 def _get_model() -> PreTrainedModel:
     return ORTModelForSequenceClassification.from_pretrained(
         "MoritzLaurer/xtremedistil-l6-h256-zeroshot-v1.1-all-33",
-        file_name="onnx/model.onnx",
+        subfolder="onnx",
+        file_name="model.onnx",
         export=False,
         local_files_only=True,
     )
@@ -56,15 +58,21 @@ def _get_classifier() -> Pipeline:
     )
 
 
+class ClassificationResults(TypedDict):
+    sequence: str
+    labels: List[str]
+    scores: List[float]
+
+
 def __get_scores_per_label(
-    text: str, topics: List[str], hypothesis_template: str = _hypothesis_template, multi_label: bool = True
-) -> Optional[Dict[str, float]]:
+    text: List[str], topics: List[str], hypothesis_template: str = _hypothesis_template, multi_label: bool = True
+) -> List[ClassificationResults]:
     if not text:
-        return None
+        return []
+
     classifier = _get_classifier()
-    result: Dict[str, [str, float]] = classifier(text, topics, hypothesis_template=hypothesis_template, multi_label=multi_label)  # type: ignore
-    scores_per_label: Dict[str, float] = {label: score for label, score in zip(result["labels"], result["scores"])}  # type: ignore[reportUnknownVariableType]
-    return scores_per_label
+    result: List[ClassificationResults] = classifier(text, topics, hypothesis_template=hypothesis_template, multi_label=multi_label)  # type: ignore
+    return result
 
 
 def _sanitize_metric_name(topic: str) -> str:
@@ -78,20 +86,16 @@ def topic_metric(input_name: str, topics: List[str], hypothesis_template: Option
     hypothesis_template = hypothesis_template or _hypothesis_template
 
     def udf(text: pd.DataFrame) -> MultiMetricResult:
-        metrics: Dict[str, List[Optional[float]]] = {topic: [] for topic in topics}
+        value: List[str] = list(UdfInput(text).iter_column_rows(input_name))
+        results = __get_scores_per_label(value, topics=topics, hypothesis_template=hypothesis_template)
 
-        def process_row(row: pd.DataFrame) -> Dict[str, List[Optional[float]]]:
-            value: Any = row[input_name]  # type: ignore
-            scores = __get_scores_per_label(value, topics=topics, hypothesis_template=hypothesis_template)  # pyright: ignore[reportUnknownArgumentType]
-            for topic in topics:
-                metrics[topic].append(scores[topic] if scores else None)
-            return metrics
-
-        text.apply(process_row, axis=1)  # pyright: ignore[reportUnknownMemberType]
-
-        all_metrics = [
-            *metrics.values(),
-        ]
+        all_metrics: List[List[float]] = [[] for _ in topics]
+        for result in results:
+            # Map each topic to its score in the current result
+            topic_to_score = {label: score for label, score in zip(result["labels"], result["scores"])}
+            # For each topic, append the score to the corresponding list in all_metrics
+            for i, topic in enumerate(topics):
+                all_metrics[i].append(topic_to_score[topic])  # Append list of score for the topic
 
         return MultiMetricResult(metrics=all_metrics)
 
